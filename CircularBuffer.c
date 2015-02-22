8ad1d781074c99f5c9d61fcfa3d29889f5905728
Jeff Schwab <jeff@orchardplatform.com>
2015-02-22 18:24:11 -0500

    Implement and test the whole kaboodle.

--- CircularBuffer.c
+++ CircularBuffer.c
@@ -5,22 +5,67 @@
 
 #include "CircularBuffer.h"
 
+#include <stdlib.h>     // for calloc, free
+
 struct CircularBuffer
 {
-    // keep compiler from complaining about an empty struct
-    int place_holder;
+    unsigned int capacity;  // cell count, including both used and unused cells
+    int * cells;            // storage for integers
+    int default_;           // returned on `get` from empty buffer
+    unsigned int in;        // index where the next `put` value will be stored
+    unsigned int size;      // count of cells currently in use
 };
 
 struct CircularBuffer * CircularBuffer_Create(unsigned int capacity, int default_value)
 {
-    struct CircularBuffer * self = (struct CircularBuffer *)calloc(1, sizeof(struct CircularBuffer));
+    struct CircularBuffer * self;       // result
+    if (NULL == (self = malloc(sizeof(struct CircularBuffer))))
+        return NULL;
+    if (NULL == (self->cells = malloc(capacity * sizeof(int)))) {
+        free(self);
+        return NULL;
+    }
+    self->capacity = capacity;
+    self->default_ = default_value;
+    self->in = 0;                       // begin at the beginning
+    self->size = 0;                     // start empty
     return self;
 }
 
 void CircularBuffer_Destroy(struct CircularBuffer * self)
 {
+    free(self->cells);
     free(self);
 }
 
+bool CircularBuffer_IsEmpty(struct CircularBuffer * self)
+{
+    return 0 == self->size;
+}
 
+bool CircularBuffer_IsFull(struct CircularBuffer * self)
+{
+    return self->capacity == self->size;
+}
 
+bool CircularBuffer_Put(struct CircularBuffer * self, int value)
+{
+    if (self->capacity == self->size)
+        return false;
+    self->cells[self->in] = value;
+    self->in = (self->in + 1) % self->capacity;
+    self->size++;
+    return true;
+}
+
+int CircularBuffer_Get(struct CircularBuffer * self)
+{
+    return self->size
+        ? self->cells[(self->capacity - self->size-- + self->in) % self->capacity]
+        : self->default_;
+}
+
+unsigned int CircularBuffer_Capacity(struct CircularBuffer * self)
+{
+    return self->capacity;
+}
--- main.c
+++ main.c
@@ -3,25 +3,86 @@
 //- Cannot be used by attendees to train others without written permission.
 //- www.wingman-sw.com james@wingman-sw.com
 
-#include <stdio.h>
 #include "CircularBuffer.h"
+
+#include <assert.h>     // for assert
+#include <errno.h>      // for errno
+#include <stdio.h>      // for perror, printf
+#include <stdlib.h>     // for exit
 
 //please do not change these messages
 const char * writing = "OK writing";
 const char * testing = "FAILED testing";
 const char * working = "OK working";
 
+
+// Test coverage:
+//
+//      ,----------------------.
+//      | Operation | State    |
+//      |===========|==========|
+//    0 | IsEmpty   | Empty    |
+//    1 |           | Partial  |
+//    2 |           | Full     |
+//      |-----------|----------|
+//    3 | IsFull    | Empty    |
+//    4 |           | Partial  |
+//    5 |           | Full     |
+//      |-----------|----------|
+//    6 | Put       | Empty    |
+//    7 |           | Partial  |
+//    8 |           | Full     |
+//      |-----------|----------|
+//    9 | Get       | Empty    |
+//   10 |           | Partial  |
+//   11 |           | Full     |
+//      |-----------|----------|
+//   12 | Capacity  | Empty    |
+//   13 |           | Partial  |
+//   14 |           | Full     |
+//      |___________|__________|
+
 int main(int ac, char** av)
 {
-    unsigned int capacity = 10;
-    int empty_value = -1;
-    const char * exercise_state = writing;
+    const unsigned int capacity = 10;
+    const int empty_value = -1;
+    const char * exercise_state = working;
+    int i;  // loop iterator
 
     struct CircularBuffer * b = CircularBuffer_Create(capacity, empty_value);
-    CircularBuffer_Destroy(b);
 
+    if (NULL == b) {
+        perror("Failed to initialize circular buffer");
+        exit(1);
+    }
+
+    assert(CircularBuffer_IsEmpty(b));                      // row  0
+    assert(empty_value == CircularBuffer_Get(b));           // row  9
+
+    // Write after Read (WaR), Write after Write (WaW)
+    for (i = 0; i < capacity; ++i) {
+        assert(!CircularBuffer_IsFull(b));                  // row  3,  4
+        assert(capacity == CircularBuffer_Capacity(b));     // row 12, 13
+        CircularBuffer_Put(b, 0xDeadBeef + i);              // row  6,  7
+        assert(!CircularBuffer_IsEmpty(b));                 // row  1,  2
+    }
+
+    assert(CircularBuffer_IsFull(b));                       // row  5
+    assert(!CircularBuffer_Put(b, 0xDeadBeef));             // row  8
+    assert(capacity == CircularBuffer_Capacity(b));         // row 14
+
+    // Read after Write (RaW), Read after Read (RaR)
+    for (i = 0; i < capacity; ++i) {
+        assert(!CircularBuffer_IsEmpty(b));                 // row  1,  2
+        assert(0xDeadBeef + i == CircularBuffer_Get(b));    // row 10, 11
+        assert(!CircularBuffer_IsFull(b));                  // row  3,  4
+    }
+
+    assert(CircularBuffer_IsEmpty(b));                      // row  0
+    assert(empty_value == CircularBuffer_Get(b));           // row  9
+
+    CircularBuffer_Destroy(b);
 
     printf("%s code\n", exercise_state);
     return 0;
 }
-
